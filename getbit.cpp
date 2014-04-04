265fff7cfb6114b868e3bd5e3c4f407ec06c3956
Tony Camuso <tcamuso@redhat.com>
2014-04-04 09:30:44 -0400

    *bit*.cpp: Bit manipulation helper sources for shell scripts
    bash does not have bit manipulation, so these C++ sources provide the
    executables for bit manipulaton that can be called from bash.
    
    These routines send ouput to stdout, so the bash script can capture
    their output with assignment, e.g.
    
    	bitmask=$(togglebit $bit $mask)

---
+++ getbit.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+// Determines if a bit is set in a mask.
+// getbit bit mask
+
+// This code converts from string to number safely.
+//
+bool str2int(string& str, int& num)
+{
+	stringstream ss(str);
+	if (ss >> num)
+		return true;
+	else
+		return false;
+}
+
+int main(int argc, char *argv[])
+{
+	int mask;
+	int bit;
+	string str;
+
+	if(argc < 3)
+		return 0;
+
+        str = argv[1];
+        if (!str2int(str, bit))
+		return 0;
+	str = argv[2];
+        if (!str2int(str, mask))
+		return 0;
+	bit = 1 << bit;
+	cout << bit & mask;
+	return bit & mask;
+}
+
