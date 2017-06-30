a19c028729ce125f398b914746f168b6411e5a03
Tony Camuso <tcamuso@redhat.com>
2017-06-30 13:49:08 -0400

    getbit.cpp: refactored into class
    Takes a decimal bit number and a hexadecimal mask without the
    leading 0x and returns a 0 if the bit number is set in the mask,
    or returns a zero if the bit number is not set.
    
    Also returns a zero if the inputs are not valid decimal and hex
    numbers, respectively.
    
    Example:
    
    getbit 10 ffff400
    echo $?
    1
    
    getbit 10 ffff4000
    echo $?
    0

--- getbit.cpp
+++ getbit.cpp
@@ -4,21 +4,53 @@
 
 using namespace std;
 
+class getbit {
+public:
+	getbit() {};
+	int run(int argc, char *argv[]);
+	bool isint(string&);
+	bool str2int(string&, int&);
+
+private:
+	bool hexdec;
+};
+
+
 // Determines if a bit is set in a mask.
 // getbit bit mask
 
-// This code converts from string to number safely.
-//
-bool str2int(string& str, int& num)
+bool getbit::isint(string& s)
 {
-	stringstream ss(str);
-	if (ss >> num)
-		return true;
-	else
-		return false;
+    bool isnumber = true;
+
+   //  cout << "hexdec: " << (hexdec ? "true" : "false") << endl;
+
+    for(string::const_iterator k = s.begin(); k != s.end(); ++k) {
+        if (hexdec)
+            isnumber = isnumber && isxdigit(*k);
+        else
+            isnumber = isnumber && isdigit(*k);
+    }
+
+    return isnumber;
 }
 
-int main(int argc, char *argv[])
+// This code converts from string to number safely.
+//
+bool getbit::str2int(string& str, int& num)
+{
+    stringstream ss(str);
+
+    if (!isint(str))
+        return false;
+
+    if (hexdec)
+        return (ss >> hex >> num);
+    else
+        return (ss >> num);
+}
+
+int getbit::run(int argc, char *argv[])
 {
 	int mask;
 	int bit;
@@ -27,14 +59,26 @@ int main(int argc, char *argv[])
 	if(argc < 3)
 		return 0;
 
+	this->hexdec = false;
         str = argv[1];
+
         if (!str2int(str, bit))
 		return 0;
+
+	this->hexdec = true;
 	str = argv[2];
-        if (!str2int(str, mask))
+
+	if (!str2int(str, mask))
 		return 0;
+
 	bit = 1 << bit;
-	cout << (bit & mask);
-	return bit & mask;
+	// cout << (bit & mask);
+	return !!(bit & mask);
+}
+
+int main(int argc, char *argv[])
+{
+	getbit gb;
+	return gb.run(argc, argv);
 }
 
