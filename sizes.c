3b4d3309d823066db2a01c95f9335e354913b43d
Tony Camuso <tcamuso@redhat.com>
2017-12-08 15:33:39 -0500

    sizes.c: sizes of native C types on running architecture
    Compile and run this to see the native sizes for C types on the
    running architecture.

---
+++ sizes.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main()
+{
+	printf ("     sizeof(char): %d\n", sizeof(char));
+	printf ("      sizeof(int): %d\n", sizeof(int));
+	printf ("     sizeof(long): %d\n", sizeof(long));
+	printf ("sizeof(long long): %d\n", sizeof(long long));
+}
