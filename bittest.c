77c54189838c02a7745b7bebc4a55482dd3a9e05
Tony Camuso <tcamuso@redhat.com>
2017-12-08 17:55:06 -0500

    bittest.c: use macros instead of funcs
    Also use div and mod instead of iteration (thanks darcari).
    Use the following to see all bits.
    
    $ for ((i=0; i<128; ++i)); do bittest $i; done

--- bittest.c
+++ bittest.c
@@ -1,40 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <inttypes.h>
 
-#define sizeofint (sizeof(int))
-#define sizeofarray 4
-#define nbits (sizeofint * sizeofarray)
-#define bits_per_byte 8
-#define bits_per_int (sizeofint * bits_per_byte)
+typedef unsigned char u8;
 
-static unsigned int intarray[sizeofarray];
+#define ACPI_MASKABLE_GPE_MAX   0x80
+#define BITS_PER_INT (sizeof(int) * 8)
+#define GPE_MASK_ARRAY_SIZE (ACPI_MASKABLE_GPE_MAX / BITS_PER_INT)
 
-void setbit(int bit)
+#define BIT_INDEX(bit) (bit / BITS_PER_INT)
+#define BIT_OFFSET(bit) (bit % BITS_PER_INT)
+
+static unsigned int acpi_masked_gpes[GPE_MASK_ARRAY_SIZE];
+
+#define acpi_gpe_mask_set_bit(bit) \
+        acpi_masked_gpes[BIT_INDEX(bit)] |= (1 << BIT_OFFSET(bit))
+
+#define acpi_gpe_mask_test_bit(bit) \
+        !!(acpi_masked_gpes[BIT_INDEX(bit)] & (1 << BIT_OFFSET(bit)))
+
+static int acpi_gpe_set_masked_gpes(char *val)
 {
-	int i;
-	int k = bit;
+	unsigned long gpe = strtoul(val, NULL, 0);
 
-	for (i = 0; i < sizeofarray; ++i) {
-		k -= bits_per_int;
-		printf("bits_per_int: %d k: %d\n", bits_per_int, k);
-		if (k < 0) break;
-	}
+	acpi_gpe_mask_set_bit(gpe);
 
-	printf ("i: %d\n", i);
-	bit -= i * bits_per_int;
-	printf ("bit: %d mask: %08x\n", bit, ((unsigned)1<<bit));
-	intarray[i] |= ((unsigned)1<<bit);
+	return 1;
 }
 
-void main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i;
+	char *cp = argv[1];
 	int bit;
 
-	sscanf(argv[1], "%d", &bit);
-	setbit(bit);
+	sscanf(cp, "%d", &bit);
 
-	for (i = 0; i < sizeofarray; ++i) {
-		printf("array[%d]: %08x\n", i, intarray[i]);
-	}
+	acpi_gpe_set_masked_gpes(cp);
+	printf("bit: %3d bitmap: %08x.%08x.%08x.%08x\n",
+		bit,
+		acpi_masked_gpes[3],
+		acpi_masked_gpes[2],
+		acpi_masked_gpes[1],
+		acpi_masked_gpes[0]);
+
+	acpi_gpe_mask_test_bit(bit);
+	return 0;
 }
