fecc9fe8f4797771c7205ea781b70848759077e6
Tony Camuso <tcamuso@redhat.com>
2017-12-09 13:50:44 -0500

    bittest.c: add test for whether bit is set

--- bittest.c
+++ bittest.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <inttypes.h>
 
+#define min(x, y)   (((x) < (y)) ? (x) : (y))
 typedef unsigned char u8;
 
 #define ACPI_MASKABLE_GPE_MAX   0x80
@@ -12,22 +13,40 @@ typedef unsigned char u8;
 #define BIT_INDEX(bit) (bit / BITS_PER_INT)
 #define BIT_OFFSET(bit) (bit % BITS_PER_INT)
 
+#define ACPI_GPE_MASK_SET_BIT(bit) \
+	acpi_masked_gpes[BIT_INDEX(bit)] |= (1 << BIT_OFFSET(bit))
+
+#define ACPI_GPE_MASK_TEST_BIT(bit) \
+	!!(acpi_masked_gpes[BIT_INDEX(bit)] & (1 << BIT_OFFSET(bit)))
+
 static unsigned int acpi_masked_gpes[GPE_MASK_ARRAY_SIZE];
-
-#define acpi_gpe_mask_set_bit(bit) \
-        acpi_masked_gpes[BIT_INDEX(bit)] |= (1 << BIT_OFFSET(bit))
-
-#define acpi_gpe_mask_test_bit(bit) \
-        !!(acpi_masked_gpes[BIT_INDEX(bit)] & (1 << BIT_OFFSET(bit)))
+static int acpi_current_gpe_count = 128;
 
 static int acpi_gpe_set_masked_gpes(char *val)
 {
-	unsigned long gpe = strtoul(val, NULL, 0);
+	unsigned gpe;
 
-	acpi_gpe_mask_set_bit(gpe);
+	gpe = strtoul(val, NULL, 0);
+	if (gpe >= ACPI_MASKABLE_GPE_MAX)
+		return -1;
+	ACPI_GPE_MASK_SET_BIT(gpe);
 
 	return 1;
 }
+
+void acpi_gpe_apply_masked_gpes(void)
+{
+	u8 gpe;
+
+	for (gpe = 0;
+	     gpe < min(ACPI_MASKABLE_GPE_MAX, acpi_current_gpe_count);
+	     gpe++) {
+		if (ACPI_GPE_MASK_TEST_BIT(gpe)) {
+			printf("Masking GPE 0x%x.\n", gpe);
+		}
+	}
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -36,7 +55,11 @@ int main(int argc, char *argv[])
 
 	sscanf(cp, "%d", &bit);
 
-	acpi_gpe_set_masked_gpes(cp);
+	if ((acpi_gpe_set_masked_gpes(cp) < 1)) {
+		printf("%s is an invalid GPE number\n", cp);
+		return 1;
+	}
+
 	printf("bit: %3d bitmap: %08x.%08x.%08x.%08x\n",
 		bit,
 		acpi_masked_gpes[3],
@@ -44,6 +67,7 @@ int main(int argc, char *argv[])
 		acpi_masked_gpes[1],
 		acpi_masked_gpes[0]);
 
-	acpi_gpe_mask_test_bit(bit);
+	acpi_gpe_apply_masked_gpes();
+
 	return 0;
 }
