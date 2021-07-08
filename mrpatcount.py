138548d59ce5436781a7784b996736b8254b02be
Tony Camuso <tcamuso@redhat.com>
2021-07-08 14:10:28 -0400

    patchtools: mrpatcount.py: script to accurately count patches in MR

---
+++ mrpatcount.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+#
+# First create a config file in either /etc or ~/
+# Contents should be as follows
+#
+# -------------------------------------------------
+# [global]
+# default = https://gitlab.com
+# ssl_verify = true
+# timeout = 5
+# 
+# [gitlab]
+# url = https://gitlab.com
+# private_token = <your private gitlab token here>
+# api_version = 4
+# -------------------------------------------------
+#
+# Then you must set an environment variable..
+# 
+#   PYTHON_GITLAB_CFG=~/.python-gitlab.cfg
+# 
+# ..or..
+#
+#   PYTHON_GITLAB_CFG=/etc/python-gitlab.cfg
+#
+# .. depending on where you created the config file.
+#
+# USAGE:
+#
+# mrpatcount 24118165 859
+#
+# Where:
+#
+#   24118165 is the project ID for ..
+#       https://gitlab.com/redhat/rhel/src/kernel/rhel-8
+#
+#   859 is an MR ID number.
+#
+
+import sys
+import gitlab
+
+project_id = str(sys.argv[1])
+mr_id   = str(sys.argv[2])
+
+# outfil  = str(sys.argv[3])
+# f = open(outfil, 'w')
+
+gl = gitlab.Gitlab.from_config('gitlab')
+gl_project = gl.projects.get(project_id)
+mr = gl_project.mergerequests.get(mr_id)
+ncommits = len(mr.commits())
+print(f'{ncommits}')
+
+# print(f'Working on {gl_project.name} MR {mr_id}')
+# print(f"MR author: {mr.author['name']}")
+# print(f'Number of commits: {ncommits}')
+
+
+# f.write(str(ncommits))
+# f.close
