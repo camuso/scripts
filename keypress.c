8824861448d966563dba78df14cdea00cb9a4c28
Tony Camuso <tcamuso@redhat.com>
2023-12-14 12:17:50 -0500

    keypress.c: fix indents
    Signed-off-by: Tony Camuso <tcamuso@redhat.com>

--- keypress.c
+++ keypress.c
@@ -1,29 +1,29 @@
 #include <ncurses.h>
 
 int main(int argc, void *argv[]) {
-    initscr(); // initialize the library
-    //cbreak();  // disable line buffering
-    raw();
-    keypad(stdscr, TRUE); // enable special keys
-    bool b_test;
-    bool b_alt;
+	initscr(); // initialize the library
+	//cbreak();  // disable line buffering
+	raw();
+	keypad(stdscr, TRUE); // enable special keys
+	bool b_test;
+	bool b_alt;
 
-    if (argc > 1) b_test = true;
+	if (argc > 1) b_test = true;
 
-    if (b_test)	printw("Press any key\n");
+	if (b_test) printw("Press any key\n");
 
-    unsigned int retkey;
-    unsigned int key;
+	unsigned int retkey;
+	unsigned int key;
 
-    while (1) {
+	while (1) {
 	b_alt = false;
 	key = getch(); // get a key press;
 
 	if (key >= KEY_MIN && key <= KEY_MAX) {
-	    if (b_test)
-		    printw("Special key pressed: %s (Key code: %d)\n", keyname(key), key);
-	    else
-		    break;
+		if (b_test)
+			printw("Special key pressed: %s (Key code: %d)\n", keyname(key), key);
+		else
+			break;
 	} else {
 		if (key == 27) {
 			b_alt = true;
@@ -34,22 +34,21 @@ int main(int argc, void *argv[]) {
 		else {
 			retkey = key;
 		}
-
-	        if (b_test)
-		    printw("Key pressed: %c (ASCII code:%d  %x)\n", retkey, (int)retkey, (int)retkey);
+			if (b_test)
+			printw("Key pressed: %c (ASCII code:%d  %x)\n", retkey, (int)retkey, (int)retkey);
 	}
 
 	if (! b_test) break;
-    }
+	}
 
-    refresh();
-    endwin(); // cleanup
+	refresh();
+	endwin(); // cleanup
 
-    if(! b_test) {
+	if(! b_test) {
 	if (b_alt) printf("%x", retkey >> 8);
 	printf("%c", (char)(retkey & 0xFF));
-    }
+	}
 
-    if (b_alt) return 1;
-    return 0;
+	if (b_alt) return 1;
+	return 0;
 }
